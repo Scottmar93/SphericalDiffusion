6867f93cd9940ad925936348e977700a78a3abda
Chao Peng <C.Peng@soton.ac.uk>
2018-11-01 17:42:21 +0000

    modify test_myfunction.py

--- test_myfunction.py
+++ test_myfunction.py
@@ -3,5 +3,5 @@ from myfunctions import *
 
 # Test
 def test_sphericalSolver():
-    x0, t0, temp = sphericalSolver(0.01, 1, 0.5, [0, 3600], 10, [50, 50], 1.172 * 10 ** (-5))
+    x0, t0, C = spherical_Solver(j, r, t, params)
     assert temp.max() > 10 and temp.min() < 50
